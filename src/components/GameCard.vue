b67dd23f4d54e1da9b3992cb64baa1d5037a2158
rrsoft <john.uzodinma@internetbrands.com>
2023-11-20 00:21:53 +0100

    Completed Memory Game

---
+++ src/components/GameCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  card: {
+    type: Object,
+    required: true
+  }
+})
+
+const emit = defineEmits(['flipCard'])
+
+function clickCard(card) {
+  emit('flipCard', card)
+}
+</script>
+
+<template>
+  <div @click="clickCard(card)" class="card">
+    <div class="card__inner" :class="{ 'card__flip': card.flipped }">
+      <div class="card__flip-front">
+        <img class="card__image" :src="'/images/default.png'" />
+      </div>
+      
+      <div :class="{'card__flip card__flip-back': card.flipped }">
+        <img class="card__image" :src="card.image" />
+      </div>
+      
+    </div>
+  </div>
+</template>
